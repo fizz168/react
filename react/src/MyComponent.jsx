ed7f8eb51244c70a2370a99e3082be9eca6ff1de
fizz168 <nheanomra@gmail.com>
2026-01-06 15:33:22 +0700

    feat: enhance MyComponent with age, number, and employment status functionalities

--- react/src/MyComponent.jsx
+++ react/src/MyComponent.jsx
@@ -6,11 +6,38 @@ const updateName = () => {
     // console.log(name);  //only update in console 
     setName("nigga"); // this one show on the page 
 }
+const [age, setAge] = useState(0);
+const incrementAge = () => {
+    setAge(age + 1);
+}
+const [number ,setNumber] = useState(10);
+const DecrementNumber = () => {
+    setNumber(number - 1);
+} 
+const [isEmploy, setIsEmploy] = useState(false);
+const EmployStatus = () => {
+    setIsEmploy(!isEmploy);
+}
+
+
 return(
+    <>
     <div>
         <p>Name: {name}</p>
         <button className='nigga' onClick={updateName}>Guess who</button>
+       
+        <p>Age: {age} </p>
+        <button className='nigga' onClick={incrementAge}>Increment Age</button>
+          
+          <p>Number: {number}</p>
+        <button className='nigga' onClick={DecrementNumber}>Decrement Number</button>
+
+        <p>Are you Employ : {isEmploy ? "yes" : "no"} </p>
+        <button className='nigga' onClick={EmployStatus}>Click this to find out</button>
     </div>
+   
+    </>
+
 );
 }
 export default MyComponent
