6b1c6c7356870fedc8b24807407c2ea9c224278f
fizz168 <nheanomra@gmail.com>
2026-01-07 21:12:22 +0700

    feat : increment and reset in component

--- react/src/MyComponent.jsx
+++ react/src/MyComponent.jsx
@@ -10,10 +10,20 @@ const [age, setAge] = useState(0);
 const incrementAge = () => {
     setAge(age + 1);
 }
+
 const [number ,setNumber] = useState(10);
 const DecrementNumber = () => {
     setNumber(number - 1);
+}
+const IncrementNumber = () => {
+    setNumber(number + 1);
 } 
+const reset = () => {
+    setNumber(0);
+}
+
+
+
 const [isEmploy, setIsEmploy] = useState(false);
 const EmployStatus = () => {
     setIsEmploy(!isEmploy);
@@ -30,7 +40,9 @@ return(
         <button className='nigga' onClick={incrementAge}>Increment Age</button>
           
           <p>Number: {number}</p>
-        <button className='nigga' onClick={DecrementNumber}>Decrement Number</button>
+        <button className='nigga' onClick={DecrementNumber} >Decrement Number</button>
+        <button className='nigga' onClick={IncrementNumber}>Increment Number</button>
+        <button className='nigga' onClick={reset}>Reset</button>
 
         <p>Are you Employ : {isEmploy ? "yes" : "no"} </p>
         <button className='nigga' onClick={EmployStatus}>Click this to find out</button>
